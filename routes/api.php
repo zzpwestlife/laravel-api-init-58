5c065964efcf5dd34d47d39e91eae44e7dde4314
ZouZhipeng <zzpwestlife@gmail.com>
2019-11-14 21:32:32 +0800

    cor

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::namespace('Api')->prefix('v1')->group(function () {
+Route::namespace('Api')->prefix('v1')->middleware('cors')->group(function () {
     Route::get('/users', 'UserController@index')->name('users.index');
     Route::get('/users/{user}', 'UserController@show')->name('users.show');
     Route::post('/users', 'UserController@store')->name('users.store');
