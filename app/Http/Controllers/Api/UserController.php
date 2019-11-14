5c99f8e11b8e5444a48c8d7344f243d453a5a10c
ZouZhipeng <zzpwestlife@gmail.com>
2019-11-14 21:26:59 +0800

    user curd and form request

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\UserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    // 返回用户列表
+    public function index()
+    {
+        //3 个用户为一页
+        $users = User::paginate(3);
+        return $users;
+    }
+
+    // 返回单一用户信息
+    public function show(User $user)
+    {
+        return $user;
+    }
+
+    // 用户注册
+    public function store(UserRequest $request)
+    {
+        User::create($request->all());
+        return '用户注册成功。。。';
+    }
+
+    // 用户登录
+    public function login(Request $request)
+    {
+        $res = Auth::guard('web')->attempt(['name' => $request->name, 'password' => $request->password]);
+        if ($res) {
+            return '用户登录成功...';
+        }
+        return '用户登录失败';
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use \Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,11 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::namespace('Api')->prefix('v1')->group(function () {
+    Route::get('/users', 'UserController@index')->name('users.index');
+    Route::get('/users/{user}', 'UserController@show')->name('users.show');
+    Route::post('/users', 'UserController@store')->name('users.store');
+    Route::post('/login', 'UserController@login')->name('users.login');
 });
