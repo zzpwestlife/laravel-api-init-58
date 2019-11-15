43ef674df2794a3191a4dcfc040c92928334ef0d
ZouZhipeng <zzpwestlife@gmail.com>
2019-11-15 08:10:24 +0800

    refresh token

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::namespace('Api')->prefix('v1')->middleware('cors')->group(function () {
-    // 有绑定模型的路由，同路径的路由需要放在没绑定路由的后面！！！
-    Route::get('/users', 'UserController@index')->name('users.index');
+    // 有绑定模型的路由，同路径的路由需要放在没绑定路由的后面！！！还可以用上路由提供的正则表达式来很好解决这个问题
     Route::post('/users', 'UserController@store')->name('users.store');
     Route::post('/login', 'UserController@login')->name('users.login');
-    Route::post('/logout', 'UserController@logout')->name('users.logout');
-    Route::get('/users/info', 'UserController@info')->name('users.info');
-    Route::get('/users/{user}', 'UserController@show')->name('users.show');
+    Route::middleware('api.refresh')->group(function () {
+        Route::get('/users', 'UserController@index')->name('users.index');
+        Route::post('/logout', 'UserController@logout')->name('users.logout');
+        Route::get('/users/info', 'UserController@info')->name('users.info');
+        Route::get('/users/{user}', 'UserController@show')->where('user', '[0-9]+')->name('users.show');
+    });
 });
